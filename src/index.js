efaf915b028dccaab55618f3f8bdd40a4ecfa651
Julio Fasolato <juliofasolato@Julios-Laptop.local>
2026-06-22 13:45:39 +0200

    Update packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import GifApp from './GifApp'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import GifApp from './GifApp';
+import './index.css';
 
-ReactDOM.render(
-    <GifApp />,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<GifApp />);
